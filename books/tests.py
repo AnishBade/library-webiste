57f782052263f5fa113b5a91ac4a71f1a83e1e7f
AnishBade <badeanish6045@gmail.com>
2023-12-02 19:23:05 +0545

    added pre-commit config and requirements.txt

--- books/tests.py
+++ books/tests.py
@@ -20,7 +20,7 @@ class BookTests(TestCase):
         self.assertEqual(self.book.author, "Tom Christie")
         self.assertEqual(self.book.isbn, "1234567890123")
 
-    def test_book_listview( self ):
+    def test_book_listview(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "excellent subtitle")
